1c4b51977a8e91a26e302415d789e3fdd8820e9c
Cormac McCann <mccannsean421@gmail.com>
2019-10-01 21:31:27 +0100

    update for player movement

--- ddtd-project/Assets/Scripts/PlayerMovement.cs
+++ ddtd-project/Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,42 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    [SerializeField] private Transform groundCheck;
+
     public Rigidbody2D rb;
     public Vector2 movement;
-    public float movementSpeed = 10f;
+    public float movementSpeed = 20f;
+    public float jumpForce = 50f;
+    private bool jump = false;
+    private bool touchingGround = false;
+    public LayerMask groundLayer;
+
 
     // Update is called once per frame
     void Update() {
         movement.x = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
+
     }
 
     void FixedUpdate() {
-        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime );
+        touchingGround = false;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, .1f, groundLayer);
+
+        if (colliders.Length > 0) {
+            touchingGround = true;
+        }
+
+        if(touchingGround) {
+            rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+        }
+
+        if(jump) {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        }
     }
 }
