234d0558212fbf4aaf67b558cf13ac29cba95915
Cormac McCann <cormacmccann@Cormacs-MacBook-Pro.local>
2019-10-07 15:07:22 +0100

    Player movement working

--- ddtd-project/Assets/Scripts/PlayerMovement.cs
+++ ddtd-project/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private Transform groundCheck;
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
 
     public Rigidbody2D rb;
     public Vector2 movement;
@@ -14,32 +15,41 @@ public class PlayerMovement : MonoBehaviour
     private bool jump = false;
     private bool touchingGround = false;
     public LayerMask groundLayer;
-
+    public bool airControl = false;
+    private Vector3 m_Velocity = Vector3.zero;
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         movement.x = Input.GetAxisRaw("Horizontal");
-        if (Input.GetButtonDown("Jump")) {
+        if (Input.GetButtonDown("Jump"))
+        {
             jump = true;
-            
+
         }
 
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         touchingGround = false;
 
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, .01f, groundLayer);
 
-        if (colliders.Length > 0) {
+        if (colliders.Length > 0)
+        {
             touchingGround = true;
         }
 
-        if(touchingGround) {
-            rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
-        }
+        // Move the character by finding the target velocity
+        Vector3 targetVelocity = new Vector2(movement.x * 10f, rb.velocity.y); //replaced move variable
 
-        if(touchingGround && jump) {
+        // And then smoothing it out and applying it to the character
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+        // The player can only jump while touching the ground
+        if (touchingGround && jump)
+        {
             touchingGround = false;
             rb.AddForce(new Vector2(0f, jumpForce));
             jump = false;
