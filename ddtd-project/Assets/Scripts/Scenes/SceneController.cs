6938f23d1220c33064defd1bfd909f75133c88cb
Cormac McCann <mccannsean421@gmail.com>
2019-10-19 17:57:09 +0100

    added scene transition

---
+++ ddtd-project/Assets/Scripts/Scenes/SceneController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+
+    void OnTriggerEnter2D (Collider2D obj) {
+        if(obj.CompareTag("Player"))
+            SceneManager.LoadScene(newLevel);
+    }
+}
