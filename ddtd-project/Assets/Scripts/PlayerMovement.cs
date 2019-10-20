6b5d644fc4a16ed49bc0aa667aba6a3f4e31d2a0
Cormac McCann <mccannsean421@gmail.com>
2019-10-20 17:02:13 +0100

    added alient & agent sprites

--- ddtd-project/Assets/Scripts/PlayerMovement.cs
+++ ddtd-project/Assets/Scripts/PlayerMovement.cs
@@ -54,6 +54,7 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("isJumping", true);
             jump = true;
         }
+
     }
 
     void FixedUpdate()
---
+++ ddtd-project/Assets/Scripts/ShootingController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Shoot() {
+        Debug.Log("Shoot");
+    }
+}
