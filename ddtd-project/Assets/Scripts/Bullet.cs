7ca6e219f78e52ecd956da351a3c1ef13f7c1bb0
Cormac McCann <cormacmccann@Cormacs-MacBook-Pro.local>
2019-10-23 14:45:36 +0100

    Added Aggent Controller

--- ddtd-project/Assets/Scripts/Bullet.cs
+++ ddtd-project/Assets/Scripts/Bullet.cs
@@ -7,6 +7,7 @@ public class Bullet : MonoBehaviour
 
     public float speed = 20f;
     public Rigidbody2D rigidbody;
+    public int damage = 40;
 
 
     // Start is called before the first frame update
@@ -15,7 +16,15 @@ public class Bullet : MonoBehaviour
         rigidbody.velocity = transform.right * speed;
     }
 
-    void OnTriggerEnter2D (Collider2D obj) {
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        AgentController agent = obj.GetComponent<AgentController>();
+
+        if (agent)
+        {
+            agent.TakeDamage(40);
+        }
         Debug.Log("Bang");
+        Destroy(gameObject);
     }
 }
---
+++ ddtd-project/Assets/Scripts/Enemy/AgentController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentController : MonoBehaviour
+{
+    public int hp = 100;
+
+    // Update is called once per frame
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+
+        if (hp <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Die");
+        Destroy(gameObject);
+    }
+}
