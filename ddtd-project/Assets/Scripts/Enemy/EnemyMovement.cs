cbdc6b5a11bdb52435d516d67670e8f050309d58
Cormac McCann <mccannsean421@gmail.com>
2019-10-28 21:20:19 +0000

    added enemy movement

---
+++ ddtd-project/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float speed = 1f;
+
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {   
+        if (Vector2.Distance(transform.position, target.position) > 3) {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime); 
+        }
+    }
+}
